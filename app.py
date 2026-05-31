7001dff710ab2de6a563380b93da9d503c894641
Vaibhav Ojha <DragonWarrior15@users.noreply.github.com>
2026-05-31 22:56:54 +0530

    changes to add a custom span, and view traces in Jaeger (#2)

--- app.py
+++ app.py
@@ -5,16 +5,28 @@ import random
 
 from fastapi import FastAPI
 
+from opentelemetry import trace
+
 # create the logger
 logger = logging.getLogger("uvicorn.error")
 
 # create the app
 app = FastAPI()
 
+# set the tracer
+tracer = trace.get_tracer(__name__)
+
 
 @app.get("/run")
 def run():
     """Generate a random number and return the response."""
-    r = random.randint(1, 20)
-    logger.info(f"Generated random int {r}")
-    return r
+
+    # to see attributes from function, we need custom spans
+    with tracer.start_as_current_span("route_run_custom_logic") as span:
+        r = random.randint(1, 20)
+
+        # set a custom span attribute
+        span.set_attribute("math.result", r)
+
+        logger.info(f"Generated random int {r}")
+        return r
