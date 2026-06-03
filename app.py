0a315b6c66601a2d10c8d1551edabf8f826bf42c
DragonWarrior15 <DragonWarrior15@users.noreply.github.com>
2026-06-04 00:39:17 +0530

    trace across multiple functions/routes

--- app.py
+++ app.py
@@ -29,4 +29,42 @@ def run():
         span.set_attribute("math.result", r)
 
         logger.info(f"Generated random int {r}")
+
+        if r < 10:
+            return run1(r)
+        else:
+            return run2(r)
+
+
+# more endpoints to run trace across multiple hops
+@app.get("/run1")
+def run1(r: int):
+    """Sets a custom field value to the double of input."""
+
+    # to see attributes from function, we need custom spans
+    with tracer.start_as_current_span("route_run1_custom_logic") as span:
+        r *= 2
+
+        # set a custom span attribute
+        span.set_attribute("function.result", r)
+
+        logger.info(f"Doubled random int to {r}")
+
+        return r
+
+
+# more endpoints to run trace across multiple hops
+@app.get("/run2")
+def run2(r: int):
+    """Sets a custom field value to the double of input."""
+
+    # to see attributes from function, we need custom spans
+    with tracer.start_as_current_span("route_run2_custom_logic") as span:
+        r //= 2
+
+        # set a custom span attribute
+        span.set_attribute("function.result", r)
+
+        logger.info(f"Halved random int to {r}")
+
         return r
